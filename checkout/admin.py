1e4fe917cab408164a9245c2122530bd8c1c5218
abi19x <k9ja1xwk@students.codeinstitute.net>
2025-06-17 12:12:13 +0100

    checkout model error fixed

--- checkout/admin.py
+++ checkout/admin.py
@@ -4,18 +4,17 @@ from .models import Order
 # Register your models here.
 
 
+@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    readonly_fields = ('order_number', 'date')
+    list_display = ('order_number', 'full_name', 'email', 'order_total', 'date')
+    readonly_fields = ('order_number', 'order_total', 'date')
 
-    fields = ('order_number', 'user_profile', 'date', 'full_name',
-              'email', 'phone_number', 'country',
-              'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'county', 'order_total')
-    
-    list_display = ('order_number', 'date', 'full_name',
-                    'order_total',)
+    fields = (
+        'order_number', 'full_name', 'email', 'phone_number',
+        'country', 'postcode', 'town_or_city',
+        'street_address1', 'street_address2', 'county',
+        'order_total', 'date'
+    )
     
     ordering = ('-date',)
 
-
-admin.site.register(Order, OrderAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from decimal import Decimal
 
 # Create your models here.
 
@@ -16,7 +17,7 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=100.00, editable=False)
 
     def _generate_order_number(self):
         """
