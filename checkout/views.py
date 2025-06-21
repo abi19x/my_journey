44fee0d302f6ade54f573190a2f836fc779bc91d
abi19x <k9ja1xwk@students.codeinstitute.net>
2025-06-21 02:26:31 +0100

    Checkout page styled and added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -7,6 +7,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51RT7XNQYzCqRJuvweCHetwqwxO9ToTlngh5RrX13kKNBZ1SB5sAQJUbS0maG0kJ8Vz4ulwt9yRPSP0toiDROyGJI00JhlE8an0',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
