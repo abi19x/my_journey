18a23b22c445fd51b72c7f461d46f0b12bd3b309
abi19x <k9ja1xwk@students.codeinstitute.net>
2025-06-21 20:03:19 +0100

    added checkout success page

--- my_journey/wsgi.py
+++ my_journey/wsgi.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
+if os.path.exists('env.py'):
+    import env
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_journey.settings')
