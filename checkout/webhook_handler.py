23204b0eeb1a4e00a9e619425c1ef0e7094cb7cd
abi19x <k9ja1xwk@students.codeinstitute.net>
2025-06-24 04:25:07 +0100

    completed confirmation emails

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+
+from checkout.models import Order  # Make sure this import exists
+import time
 
 
 class StripeWH_Handler:
@@ -6,6 +12,23 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+    
+    def _send_confirmation_email(self, order):
+        """Send the user a confirmation email"""
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )        
 
     def handle_event(self, event):
         """
@@ -19,6 +42,26 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
+        intent = event.data.object
+        pid = intent.id
+        metadata = intent.metadata
+        order_number = metadata.get('order_number')  # Assuming you added this metadata when creating the payment intent
+
+        try:
+            order = Order.objects.get(order_number=order_number)
+        except Order.DoesNotExist:
+            # Give it a few seconds in case the order hasn't saved to DB yet
+            for attempt in range(5):
+                time.sleep(1)
+                try:
+                    order = Order.objects.get(order_number=order_number)
+                    break
+                except Order.DoesNotExist:
+                    order = None
+
+        if order:
+            self._send_confirmation_email(order)
+    
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
